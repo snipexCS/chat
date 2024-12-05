109b8365768960a7e07d610086ae4da50a727a44
ME <ME>
2024-12-05 16:48:22 +0800

    final version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
-import './App.css';
-import { Auth } from './components/Auth';
+import { useState } from "react";
+import "./App.css";
+import { Auth } from "./components/Auth";
+import Cookies from "universal-cookie";
+const cookie = new Cookies();
 
 function App() {
+  const [auth, setAuth] = useState(cookie.get("auth-token"));
+  const [room, setRoom] = useState();
+  if (!auth) {
+    return (
+      <div>
+        <Auth></Auth>
+      </div>
+    );
+  }
+
   return (
-    <div><Auth></Auth></div>
+    <div>
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label>Enter room name</label>
+          <input></input>
+          <button>Enter chat</button>
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,17 +1,21 @@
-import {auth,provider} from "../firebase-config.js"
-import {signInWithPopup} from "firebase/auth"
-export const Auth = () =>{
-
-    const signInWithGoogle =async () =>{
-       const result =  await signInWithPopup(auth,provider)
-              
+import { auth, provider } from "../firebase-config.js";
+import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
+const cookie = new Cookies();
+export const Auth = () => {
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookie.set("auth-token", result.user.refreshToken);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-
-    return(
-    <div className="auth"><p>
-        Sign in With Google</p>
-        <button onClick={signInWithGoogle}>DO it </button>
-        </div>
-    )
-}
+  return (
+    <div className="auth">
+      <p>Sign in With Google</p>
+      <button onClick={signInWithGoogle}>DO it </button>
+    </div>
+  );
+};
